1f85111835f170291f1527230cabd3a324a3b782
Shubham91999 <skulka86@asu.edu>
2024-12-09 22:33:35 -0700

    27. Remove Element

---
+++ Remove-Element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        tmp = []
+        for num in nums:
+            if num == val:
+                continue
+            tmp.append(num)
+        for i in range(len(tmp)):
+            nums[i] = tmp[i]
+        return len(tmp)
