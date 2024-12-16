d83f8e8a5f92af23d0c480282297f77def46ddc5
Shubham91999 <skulka86@asu.edu>
2024-12-16 15:54:41 -0700

    217. Contains Duplicate

--- Contains-Duplicate.py
+++ Contains-Duplicate.py
@@ -4,9 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        my_set = set()
-        for i in nums:
-            if i in my_set:
-                return True
-            my_set.add(i)
+        """
+        1. Brute Force Approach
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
         return False
+        """
+
+        """
+        2. Sorting Approach
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+        """
+
+        """
+        3. Using Hashset
+        myset = set()
+        for i in range(len(nums)):
+            if nums[i] in myset:
+                return True
+            myset.add(nums[i])
+        return False
+        """
+        
+        # 4. Checking HashSet Length with array length
+        return len(set(nums)) < len(nums)
