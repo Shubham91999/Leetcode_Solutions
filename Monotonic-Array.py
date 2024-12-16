bfae8ed7717d69098db63b9286a762a77f242d3a
Shubham91999 <skulka86@asu.edu>
2024-12-15 21:10:51 -0700

    896. Monotonic Array

---
+++ Monotonic-Array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        increase = True
+        for i in range(1, n):
+            if nums[i] < nums[i - 1]:
+                increase = False
+                break
+        
+        if increase:
+            return True
+
+        decrease = True
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                decrease = False
+                break
+        return decrease
