bf6eff2f63ed616dbf035b709beaeac548ef68c3
Shubham91999 <skulka86@asu.edu>
2024-12-17 20:50:05 -0700

    1. Two Sum

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,15 +1,52 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        #Creating a hashmap
-        hashmp = {}
+        """
+        1. Nested For Loop
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+        """
 
-        #Storing all list elements in hashmap
+        """
+        2. Sorting
+        A = []
         for i, num in enumerate(nums):
-            hashmp[num] = i
+            A.append([num, i])
+        A.sort()
 
-        #Traversing list to calculate difference between target and element
-        for i, num in enumerate(nums):
-            if target-num in hashmp and hashmp[target-num] != i:
-                return [i, hashmp[target-num]]
+        i, j = 0, len(nums) - 1
+        while i < j:
+            cur = A[i][0] + A[j][0]
+            if cur == target:
+                return [min(A[i][1], A[j][1]),
+                        max(A[i][1], A[j][1])]
+            elif(cur < target):
+                i += 1
+            else: 
+                j -= 1
+        return []
 
-        return[]
+        """
+
+        """"
+        3. HashMap Two pass approach
+        indices = {}
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]] 
+
+        """
+
+        # 4. Hashmap One Pass approach
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
