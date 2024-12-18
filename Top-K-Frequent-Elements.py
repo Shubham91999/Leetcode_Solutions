ee6f072b0c85783dde4b3f833e620264c9c72c0b
Shubham91999 <skulka86@asu.edu>
2024-12-18 12:41:51 -0700

    347. Top K Frequent Elements

--- Top-K-Frequent-Elements.py
+++ Top-K-Frequent-Elements.py
@@ -6,17 +6,57 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        """
+        1. Sorting approach with o(n log n)
         count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-
         for num in nums:
             count[num] = 1 + count.get(num, 0)
+
+        arr = []
+        for num, cnt in count.items():
+            arr.append([cnt, num])
+        arr.sort()
+
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        return res
+        """
+
+        """
+        2. Using Min Heap
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+        """
+
+        # 3. Bucket Sort approach
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        freq = [[] for i in range(len(nums) + 1)]
         for num, cnt in count.items():
             freq[cnt].append(num)
-        
+
         res = []
-        for i in range(len(freq) - 1, 0, -1):
+        for i in range(len(freq)-1, 0, -1):
             for num in freq[i]:
                 res.append(num)
                 if len(res) == k:
                     return res
+
+
+
+
