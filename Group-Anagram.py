05be401cf273c4e6ce131a105b2e1ef9e73a7be2
Shubham91999 <skulka86@asu.edu>
2024-12-17 21:36:20 -0700

    49. Group Anagrams

--- Group-Anagram.py
+++ Group-Anagram.py
@@ -1,11 +1,19 @@
 class Solution(object):
     def groupAnagrams(self, strs):
+        
         """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+        1. Sorting string and grouping together
         res = defaultdict(list)
         for s in strs:
             sortedS = ''.join(sorted(s))
             res[sortedS].append(s)
+        return res.values()"""
+
+        #. Hashtable with elphabet count as key
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
         return list(res.values())
