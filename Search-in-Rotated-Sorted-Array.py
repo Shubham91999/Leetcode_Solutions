fb2ae1a023aa7ac5009f0cf74970c8b6dde62ff8
Shubham91999 <skulka86@asu.edu>
2025-01-16 22:43:45 -0700

    33. Search in Rotated Sorted Array

--- Search-in-Rotated-Sorted-Array.py
+++ Search-in-Rotated-Sorted-Array.py
@@ -1,29 +1,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        # 1. Brute Force 
+        """
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+        """
 
-        while l < r:
-            m = (l + r) // 2
-            if nums[m] > nums[r]:
-                l = m + 1
-            else:
-                r = m
-
-        pivot = l
+        # 2. Binary Search Approach
+        if len(nums) == 1:
+            if nums[0] == target:
+                return 0
         
-        def binary_search(left: int, right: int) -> int:
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] < target:
-                    left = mid + 1
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            elif nums[mid] >= nums[low]:
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid - 1
                 else:
-                    right = mid - 1
-            return -1
-
-        result = binary_search(0, pivot - 1)
-        if result != -1:
-            return result
-        
-        return binary_search(pivot, len(nums) - 1)
+                    low = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
