320bc44384a4e10342a9fb972b6442177ec2daa6
Shubham91999 <skulka86@asu.edu>
2025-02-06 17:31:20 -0700

    141. Linked List Cycle

--- Linked-List-Cycle.py
+++ Linked-List-Cycle.py
@@ -6,11 +6,23 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
-        cur = head
-        while cur:
-            if cur in seen:
+        # seen = set()
+        # curr = head
+
+        # while curr:
+        #     if curr in seen:
+        #         return True
+        #     seen.add(curr)
+        #     curr = curr.next
+
+        # return False
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
-            seen.add(cur)
-            cur = cur.next
         return False
