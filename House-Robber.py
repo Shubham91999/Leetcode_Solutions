d669f8544b0b7e41c8f71b1776691a16712128d1
Shubham91999 <skulka86@asu.edu>
2025-03-02 14:15:44 -0700

    198. House Robber

--- House-Robber.py
+++ House-Robber.py
@@ -1,13 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = [-1] * len(nums)
+        # memo = [-1] * len(nums)
 
-        def dfs(i):
-            if i >= len(nums):
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
-            return memo[i]
+        # def dfs(i):
+        #     if i >= len(nums):
+        #         return 0
+        #     if memo[i] != -1:
+        #         return memo[i]
+        #     memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+        #     return memo[i]
         
-        return dfs(0)
+        # return dfs(0)
+
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+
