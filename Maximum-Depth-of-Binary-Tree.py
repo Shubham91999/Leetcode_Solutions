9d86d2d9431084f60b034d445dd9a343e2e9e1cb
Shubham91999 <skulka86@asu.edu>
2025-03-05 07:09:45 -0700

    104. Maximum Depth of Binary Tree

--- Maximum-Depth-of-Binary-Tree.py
+++ Maximum-Depth-of-Binary-Tree.py
@@ -6,14 +6,43 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        stack = [[root, 1]]
-        res = 0
+        # 1. Iterative BFS (Using Queue)
+        # if not root:
+        #     return 0
+        # queue = deque([root])
+        # level = 0
 
-        while stack:
-            node, depth = stack.pop()
+        # while queue:
+        #     for i in range(len(queue)):
+        #         node = queue.popleft()
 
-            if node:
-                res = max(res, depth)
-                stack.append([node.left, depth + 1])
-                stack.append([node.right, depth + 1])
-        return res
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)      
+        #     level += 1
+
+        # return level
+
+        # # 2. Iterative DFS (Stack)
+        # if not root:
+        #     return 0
+        # stack = [[root, 1]]
+        # res = 1
+
+        # while stack:
+        #     node, depth = stack.pop()
+
+        #     res = max(res, depth)
+        #     if node.left:
+        #         stack.append([node.left, depth + 1])
+        #     if node.right:
+        #         stack.append([node.right, depth + 1])
+
+        # return res
+
+        # 3. Recursion
+        if not root:
+            return 0
+        else:
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
