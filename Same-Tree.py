265bb80e5cc1da0b62fdc8a4079513cc3d11f755
Shubham91999 <skulka86@asu.edu>
2025-03-05 08:25:53 -0700

    100. Same Tree

--- Same-Tree.py
+++ Same-Tree.py
@@ -6,21 +6,56 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # 1. Recursion (DFS)
+        # # If both trees null, return True
+        # if not p and not q:
+        #     return True
+        
+        # # If one of the tree is null or values not same, return False
+        # if not p or not q or p.val != q.val:
+        #     return False
+
+        # # Recursive call to check subtrees
+        # return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+        # Iterative DFS (Using stack)
+        # Initializing stack
+        stack = [(p, q)] # Tree refs assigned as tuple 
+
+        # Pop nodes till stack has elements
+        # while stack:
+        #     node1, node2 = stack.pop()
+
+        #     # if both nodes null, skip the iteration to check remaining nodes
+        #     if not node1 and not node2:
+        #         continue
+        #     # if one of the node is null and value is differenr, return false
+        #     if not node1 or not node2 or node1.val != node2.val:
+        #         return False
+
+        #     # Add child nodes for further comparision
+        #     stack.append((node1.left, node2.left))
+        #     stack.append((node1.right, node2.right))
+
+        # return True
+
+        # 3. Iterative BFS (Queue)
         q1 = deque([p])
         q2 = deque([q])
 
         while q1 and q2:
-            nodeP = q1.popleft()
-            nodeQ = q2.popleft()
+            for _ in range(len(q1)):
+                node1 = q1.popleft()
+                node2 = q2.popleft()
 
-            if nodeP is None and nodeQ is None:
-                continue
-            if nodeP is None or nodeQ is None or nodeP.val != nodeQ.val:
-                return False
+                if not node1 and not node2:
+                    continue
+                if not node1 or not node2 or node1.val != node2.val:
+                    return False
 
-            q1.append(nodeP.left)
-            q1.append(nodeP.right)
-            q2.append(nodeQ.left)
-            q2.append(nodeQ.right)
-
+                q1.append(node1.left)
+                q1.append(node1.right)
+                q2.append(node2.left)
+                q2.append(node2.right)
         return True
+
