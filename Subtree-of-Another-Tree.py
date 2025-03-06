3544ac9d3c844792b0503f1b66ba8fc455ee71b5
Shubham91999 <skulka86@asu.edu>
2025-03-06 07:41:51 -0700

    572. Subtree of Another Tree

--- Subtree-of-Another-Tree.py
+++ Subtree-of-Another-Tree.py
@@ -6,20 +6,49 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
-            return True
-        if not root:
-            return False
+        def serialize(node):
+            if not node:
+                return "N"
+            return f"({node.val}, {serialize(node.left)}, {serialize(node.right)})"
 
-        if self.sameTree(root, subRoot):
-            return True
-        return (self.isSubtree(root.left, subRoot) or 
-               self.isSubtree(root.right, subRoot))
+        r = serialize(root)
+        sr = serialize(subRoot)
+        return sr in r
 
-    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot:
-            return True
-        if root and subRoot and root.val == subRoot.val:
-            return (self.sameTree(root.left, subRoot.left) and 
-                   self.sameTree(root.right, subRoot.right))
-        return False
+
+
+
+    #     # Taking care of base cases
+    #     # If given subtree is empty
+    #     if not subRoot:
+    #         return True
+        
+    #     # Checking main tree (as subtree is already checked, not checking again)
+    #     if not root:
+    #         return False
+
+    #     # If both conditions are not met, it means both trees are present, compare further
+        
+    #     # Calling sameTree function on root node and given subtree
+    #     if self.sameTree(root, subRoot):
+    #         return True
+
+    #     # Calling subTree function recrusively on children of root tree and given subtree
+    #     return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+
+        
+    # # Writing a helper function to compare the trees
+    # def sameTree(self, s: TreeNode, t: TreeNode) -> bool:
+    #     # Checking if both trees are empty
+    #     if not s and not t:
+    #         return True
+
+    #     # Now, we know both trees are not empty
+    #     # Making sure both trees have values and the values are same
+    #     if s and t and s.val == t.val:
+    #         # above we checked only current node, we also need to compare their child nodes
+    #         # Recursive call to check subtrees 
+    #         return (self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right))
+
+    #     return False
