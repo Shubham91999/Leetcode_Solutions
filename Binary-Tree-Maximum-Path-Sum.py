f7c39e0949d7ff9392d178c964772aa2c1580f23
Shubham91999 <skulka86@asu.edu>
2025-03-11 07:20:37 -0700

    124. Binary Tree Maximum Path Sum

--- Binary-Tree-Maximum-Path-Sum.py
+++ Binary-Tree-Maximum-Path-Sum.py
@@ -4,22 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        # Global variable to store max path
         res = [root.val]
 
+        # Helper function dfs to compute max path
         def dfs(root):
             if not root:
                 return 0
 
-            leftMax = dfs(root.left)
-            rightMax = dfs(root.right)
-            leftMax = max(leftMax, 0)
-            rightMax = max(rightMax, 0)
+            max_left = dfs(root.left)
+            max_left = max(max_left, 0)
 
-            res[0] = max(res[0], root.val + leftMax + rightMax)
-            return root.val + max(leftMax, rightMax)
+            max_right = dfs(root.right)
+            max_right = max(max_right, 0)
+
+            """ Computing max path sum WITH the split, 
+            useful incase upper node values are lesser and right child values are greater, 
+            so non need of splitting at upper nodes"""
+            res[0] = max(res[0], root.val + max_left + max_right)
+
+            # Returning max sum without split because of probability of split in upper nodes for max sum
+            return root.val + max(max_left, max_right)
 
         dfs(root)
         return res[0]
-    
