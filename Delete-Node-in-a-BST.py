fc62ea0ca2bd48857bfb5094db0936681f208d6d
Shubham91999 <skulka86@asu.edu>
2025-03-13 08:19:33 -0700

    450. Delete Node in a BST

--- Delete-Node-in-a-BST.py
+++ Delete-Node-in-a-BST.py
@@ -5,60 +5,79 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-        # # If root not present return none
-        # if not root:
-        #     return None
+        def deleteNode(self, root: Optional[TreeNode], key: int)-> Optional[TreeNode]:
+        # 1. Recursion
+        #     # Checking base condition, if root present or not
+        #     if not root:
+        #         return None
+        #     if key < root.val:
+        #         root.left = self.deleteNode(root.left, key) # If value to be deleted is in left subtree     
+        #     elif key > root.val:
+        #         root.right = self.deleteNode(root.right, key) # If value to be deleted is in right subtree
+        #     # Key is not less or greater than root value, it means we found the value to be deleted
+        #     else:
+        #         # Root found with only one child
+        #         if not root.left:
+        #             return root.right 
+        #         elif not root.right:
+        #             return root.left
+        #         # Root found with both children
+        #         """In this case we have two options
+        #         1) Find minimum from right subtree  
+        #         2) Find maximum from left subtree
+        #          """
+        #         curr = root.right  # Assigning root's right subtree to curr as we are going to get minimum from right subtree
+        #         while curr.left:
+        #             curr = curr.left # Minimum from right subtree found
+        #         root.val = curr.val # Replacing root's value with minimum from right subtree
+        #         # Now, there will be two nodes with same value, to avoid duplication
+        #         # call delete on right subtree as we found minimum from right with root value
+        #         root.right = self.deleteNode(root.right, root.val)
+
+        #     return root
+
+        # 2. Iteration
+            if not root:
+                return None
+            # Node to be deleted found at root position
+            if root.val == key:
+                return self.helper(root) # Helper function will join two hanging subtree after deleteion of root node
+
+            curr = root # Storing reference in case key not found in tree
+
+            # Key not found at root, searching in left or right subtree
+            while root:
+                # Key is lesser than root node
+                if key < root.val:
+                    # If found in left subtree, call helper with root left
+                    if root.left and root.left.val == key:
+                        root.left = self.helper(root.left)
+                        break
+                    root = root.left # Checking iteratively in left subtree
+                else:
+                    # in else, it means key is greater than root node
+                    if root.right and root.right.val == key:
+                        root.right = self.helper(root.right)
+                        break
+                    root = root.right
+            return curr
+
+        def helper(self, root):
+            if not root.left:
+                return root.left
+            if not root.right:
+                return root.left
+            # Both children present for root(to be deleted) node
+            left = root.left # Taking ref of left subtree 
+            right = root.right # Taking ref of right subtree 
+
+            # We are going to attached right subtree to the max node in left subtree
+            # Finding max node from left subtree
+            while left.right:
+                left = left.right # Now, left points to the max (rightmost) node in left subtree
+            left.right = right # Attaching right subtree to max in left
+            return root.left # returning root's left as we have attached right subtree in it
+
+
+
         
-        # # Compare key value with current node value
-        # if key > root.val:
-        #     root.right = self.deleteNode(root.right, key) #if key is greater than current val, calling deleteNode on right subtree
-        # elif key < root.val: 
-        #     root.left = self.deleteNode(root.left, key) #if key is less, calling deleteNode on left subtree with key
-        # else:
-        #     if not root.left: #if left subtree is not present, return right subtree
-        #         return root.right
-        #     elif not root.right: # if right subtree not present, return left subtree
-        #         return root.left
-
-        #     # We reached here means, found the node with key value
-        #     # Finding min in right subtree of root
-        #     curr = root.right
-        #     while curr.left:
-        #         curr = curr.left
-        #     root.val = curr.val
-        #     root.right = self.deleteNode(root.right, root.val)
-        # return root
-
-    def helper(self, root):
-        if not root.right:
-            return root.left
-        if not root.left:
-            return root.right
-        right = root.right
-        left = root.left
-        # find largest on left
-        while left.right:
-            left = left.right
-        left.right = right
-        return root.left
-    
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-        if root.val == key:
-            return self.helper(root)
-        curr = root
-        while root:
-            if key < root.val:
-                if root.left and root.left.val == key:
-                    root.left = self.helper(root.left)
-                    break
-                root = root.left
-            else:
-                if root.right and root.right.val == key:
-                    root.right = self.helper(root.right)
-                    break
-                root = root.right
-        return curr
-            
-
