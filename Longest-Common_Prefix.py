4d82a09663edbad46dadd4a365096f3496ce925f
Shubham91999 <skulka86@asu.edu>
2025-03-24 09:57:57 -0400

    14. Longest Common Prefix

--- Longest-Common_Prefix.py
+++ Longest-Common_Prefix.py
@@ -1,9 +1,6 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # 1. Vertical Scanning
         res = ""
         for i in range(len(strs[0])):
             for s in strs:
@@ -11,3 +8,15 @@ class Solution(object):
                     return res
             res += strs[0][i]
         return res
+
+        # 2. Horizontal Scanning
+        # prefix = strs[0]
+        # for i in range(1, len(strs)):
+        #     j = 0
+        #     while j < min(len(prefix), len(strs[i])):
+        #         if prefix[j] != strs[i][j]:
+        #             break
+        #         j += 1
+        #     prefix = prefix[:j]
+        # return prefix
+
