5a4e93077a349b9a1e98ee4544655ce0be5fdc23
Shubham91999 <skulka86@asu.edu>
2025-03-24 13:31:26 -0400

    20. Valid Parentheses

--- Valid-Parantheses.py
+++ Valid-Parantheses.py
@@ -1,29 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        """
-        1. Brute Force Approach
-        while '()' in s or '[]' in s or '{}' in s:
-            s = s.replace('()', '')
-            s = s.replace('[]', '')
-            s = s.replace('{}', '')
+        # 1. Brute Force Approach
+        # while '()' in s or '[]' in s or '{}' in s:
+        #     s = s.replace('()', '')
+        #     s = s.replace('[]', '')
+        #     s = s.replace('{}', '')
+        # return len(s) == 0
 
-        return s == ''
-        """
         # 2. Using Stack
         stack = []
-        closeToOpen = {
-            ")" : "(",
-            "]" : "[", 
-            "}" : "{"
-        }
+        closeToOpen = {')':'(', ']':'[', '}':'{'}
 
         for c in s:
+            # If current character is closing parantheses, check further 
             if c in closeToOpen:
-                if stack and closeToOpen[c] == stack[-1]:
+                # If stack has elements and top of stack is matching with closing parantheses(current char's value in hashmap)
+                if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
                 else:
                     return False
             else:
+                # If current character is opening parantheses add it in stack
                 stack.append(c)
+        return len(stack) == 0 # Stack length 0 indicates parantheses matched and got popped
 
-        return  len(stack) == 0        
