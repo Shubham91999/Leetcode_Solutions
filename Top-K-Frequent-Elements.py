8b2e99977673d40a9dc43b9425282635b61a1dea
Shubham91999 <skulka86@asu.edu>
2025-03-25 10:24:47 -0400

    347. Top K Frequent Elements

--- Top-K-Frequent-Elements.py
+++ Top-K-Frequent-Elements.py
@@ -1,61 +1,46 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        
-        """
-        1. Sorting approach with o(n log n)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. Brute Force
+        # res = {}
+        # unique = set(nums)
+
+        # for num in unique:
+        #     res[num] = nums.count(num)
+        # res = dict(sorted(res.items(), key= lambda item: item[1], reverse=True)[:k])
+        # #print(list(res.keys()))
+        # return list(res.keys())
+
+        # 2. Using MinHeap
+        # count = {}
+        # for num in nums:
+        #     count[num] = 1 + count.get(num, 0)
+        # heap = []
+        # for num in count.keys():
+        #     heapq.heappush(heap, (count[num], num))
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+        # res = []
+        # for i in range(k):
+        #     res.append(heapq.heappop(heap)[1])
+        # return res
+
+        # 3. Modified Bucket Sort
         count = {}
+        freq = [[] for i in range(len(nums)+1)]
+        # Hashmap of num : count pairs
         for num in nums:
             count[num] = 1 + count.get(num, 0)
-
-        arr = []
-        for num, cnt in count.items():
-            arr.append([cnt, num])
-        arr.sort()
-
-        res = []
-        while len(res) < k:
-            res.append(arr.pop()[1])
-        return res
-        """
-
-        """
-        2. Using Min Heap
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-
-        heap = []
-        for num in count.keys():
-            heapq.heappush(heap, (count[num], num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        res = []
-        for i in range(k):
-            res.append(heapq.heappop(heap)[1])
-        return res
-        """
-
-        # 3. Bucket Sort approach
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-
-        freq = [[] for i in range(len(nums) + 1)]
+        # Frequency list with list of numbers at their count = index in freq position
         for num, cnt in count.items():
             freq[cnt].append(num)
-
         res = []
         for i in range(len(freq)-1, 0, -1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+            for n in freq[i]:
+                res.append(n)
+            if len(res) == k:
+                return res
+            
+        
 
 
 
