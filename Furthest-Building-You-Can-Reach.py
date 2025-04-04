a4bcf4015ad332e548ea72b3e579808ff61df4df
Shubham91999 <skulka86@asu.edu>
2025-04-03 21:39:06 -0700

    1642. Furthest Building You Can Reach

--- Furthest-Building-You-Can-Reach.py
+++ Furthest-Building-You-Can-Reach.py
@@ -1,21 +1,57 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        ladder_allocations = [] # We'll use heapq to treat this as a min-heap.
-        for i in range(len(heights) - 1):
-            climb = heights[i + 1] - heights[i]
-            # If this is actually a "jump down", skip it.
-            if climb <= 0:
+        # 1. Using MinHeap
+        # ladder_allocations = []  # Minheap
+        # for i in range(len(heights)-1):
+        #     climb = heights[i+1] - heights[i] # Difference between current and next building
+        #     if climb <= 0: # if next building is lesser in height, no change
+        #         continue
+        #     heapq.heappush(ladder_allocations, climb) # if greater, add climb in heap
+        #     if len(ladder_allocations) <= ladders: # if heap size is less than number of ladders
+        #         continue
+        #     # if heap size becomes greater than number of ladders, 
+        #     # subtract the top (min climb) of heap from bricks, automatically increading the number of ladders remaining
+        #     bricks -= heapq.heappop(ladder_allocations)
+        #     # while subtracting, if number of bricks become less than 0, 
+        #     # return current position as no next climb possible, heap is already size of ladders so no ladders remaining 
+        #     if bricks < 0:
+        #         return i
+        # return len(heights) - 1
+
+        # 2. Using MaxHeap
+        heap = []
+        for i in range(len(heights)-1):
+            diff = heights[i+1] - heights[i]
+            if diff <= 0:
                 continue
-            # Otherwise, allocate a ladder for this climb.
-            heapq.heappush(ladder_allocations, climb)
-            # If we haven't gone over the number of ladders, nothing else to do.
-            if len(ladder_allocations) <= ladders:
-                continue
-            # Otherwise, we will need to take a climb out of ladder_allocations
-            bricks -= heapq.heappop(ladder_allocations)
-            # If this caused bricks to go negative, we can't get to i + 1
+            bricks -= diff
+            heapq.heappush(heap, -diff)
             if bricks < 0:
-                return i
-        # If we got to here, this means we had enough to cover every climb.
-        return len(heights) - 1
+                if ladders == 0:
+                    return i
+                ladders -= 1
+                bricks += -heapq.heappop(heap)
+        return len(heights)-1
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 
