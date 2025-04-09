bfb548ffcdf6c9239cfbf72f1f2e87407fa5cc71
Shubham91999 <skulka86@asu.edu>
2025-04-09 13:50:56 -0700

    767. Reorganize String

--- Reorganize-String.py
+++ Reorganize-String.py
@@ -1,36 +1,72 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        # Calculate the length of the string
-        string_length = len(s)
-      
-        # Count the frequency of each character in the string
-        char_count = Counter(s)
-      
-        # Find the maximum frequency of any character
-        max_freq = max(char_count.values())
-      
-        # If the max frequency is more than half of the string length, round up,
-        # then the task is impossible as that character would need to be adjacent to itself.
-        if max_freq > (string_length + 1) // 2:
-            return ''
-      
-        # Initialize index for placing characters
-        index = 0
-      
-        # Create a list to store the reorganized string
-        reorganized = [None] * string_length
-      
-        # Fill in the characters, starting with the most common
-        for char, freq in char_count.most_common():
-            while freq:
-                # Place the character at the current index
-                reorganized[index] = char
-                # Decrease the frequency count
-                freq -= 1
-                # Move to the next even index or the first odd index if the end is reached
-                index += 2
-                if index >= string_length:
-                    index = 1
-      
-        # Return the list as a string
-        return ''.join(reorganized)
+        # 1. Frequency Count approach
+        # freq = [0] * 26 # Frequency array to store count of 26 alphabets
+        # for char in s:
+        #     freq[ord(char) - ord('a')] += 1 # Incrementing counter if based in char
+        # max_freq = max(freq) 
+        # if max_freq > (len(s) + 1) // 2: # If any character appears more than half of len of s + 1, it is impossible to reorganize them
+        #     return ""
+        
+        # res = [] # List to store result
+        # while len(res) < len(s):  # Loop till lenght of result is less than given string
+        #     # we will append two chars in one iteration
+        #     # Append char with max frequency
+        #     maxIDx = freq.index(max(freq)) # got max freq
+        #     char = chr(maxIDx + ord('a'))  # got character based on freq using chr method
+        #     res.append(char) # appending first char
+
+        #     freq[maxIDx] -= 1  # Decrement freq after appending
+        #     if freq[maxIDx] == 0:  # if freq is equal to 0, continue to next iteration
+        #         continue
+
+        #     # Appending char with second most freq
+        #     tmp = freq[maxIDx]
+        #     freq[maxIDx] = float("-inf") # temporarily making freq of most freq -inf to get second most freq
+        #     nextMaxIDx = freq.index(max(freq)) 
+        #     char = chr(nextMaxIDx + ord('a'))
+        #     res.append(char) # Appended second char
+
+        #     freq[maxIDx] = tmp
+        #     freq[nextMaxIDx] -= 1
+        # return ''.join(res)
+
+        # 2. Max-Heap
+        freq_map = Counter(s)
+        max_heap = [(-cnt, char) for char, cnt in freq_map.items()]
+        heapq.heapify(max_heap)
+
+        res = []
+
+        while len(max_heap) > 1:
+            # Popping two most frequent chars
+            cnt1, char1 = heapq.heappop(max_heap)
+            cnt2, char2 = heapq.heappop(max_heap)
+
+            res.extend([char1, char2])
+
+            if -cnt1 > 1:
+                heapq.heappush(max_heap, (cnt1+1, char1))
+            if -cnt2 > 1:
+                heapq.heappush(max_heap, (cnt2+1, char2))
+            
+        # if last pair remaining in heap
+        if max_heap:
+            cnt, char = heapq.heappop(max_heap)
+            if -cnt > 1:
+                return ""
+            res.append(char)
+
+        return ''.join(res)
+
+
+        
+
+
+
+
+
+
+
+
+
