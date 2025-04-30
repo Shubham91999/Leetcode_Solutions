f8bcd991fd9225daee2503aa09154ed589f6f22a
Shubham91999 <skulka86@asu.edu>
2025-04-30 13:52:53 -0700

    79. Word Search

--- Word-Search.py
+++ Word-Search.py
@@ -1,24 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS = len(board), len(board[0])
+        ROWS, COLS = len(board), len(board[0])  # Getting number of rows and columns
+        path = set()  # Mentioned, we may not used same char more than once
 
-        def dfs(r, c, i):
-            if i == len(word):
+        def dfs(r, c, i): # Nested function to pass arguments as required
+            if i == len(word): # If we reach end of the word, it means we found all chars in word
                 return True
-            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
-                word[i] != board[r][c] or board[r][c] == '#'):
+
+            # Checking three conditions:
+            # 1. Updated values of r and c are in bounds
+            # 2. Current char from word is matching with current char from board
+            # 3. Char at index (r,c) is already visited
+            if r not in range(ROWS) or c not in range(COLS) or word[i] != board[r][c] or (r, c) in path:
                 return False
-
-            board[r][c] = '#'
-            res = (dfs(r + 1, c, i + 1) or
-                   dfs(r - 1, c, i + 1) or
-                   dfs(r, c + 1, i + 1) or
-                   dfs(r, c - 1, i + 1))
-            board[r][c] = word[i]
+            # Adding it in set path, to avoid visiting again
+            path.add((r,c))
+            res = (dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1))
+            # Removing from path after processing
+            path.remove((r,c))
             return res
-
+        # Running dfs for all the chars in matrix
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r, c, 0):
                     return True
+
         return False
+
