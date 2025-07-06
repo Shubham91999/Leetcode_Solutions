602f37f9c627ed7c87ddb141ce425a9af1669bc2
Shubham91999 <skulka86@asu.edu>
2025-07-06 08:34:14 -0700

    417. Pacific Atlantic Water Flow

--- Pacific-Atlantic-Water-Flow.py
+++ Pacific-Atlantic-Water-Flow.py
@@ -1,32 +1,32 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
+        rows, cols = len(heights), len(heights[0])
         pac, atl = set(), set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def dfs(r, c, visit, prevHeight):
-            if ((r, c) in visit or 
-                r < 0 or c < 0 or 
-                r == ROWS or c == COLS or 
-                heights[r][c] < prevHeight
-            ):
-                return
+            if ((r, c) in visit or (r not in range(rows) or (c not in range(cols)) or heights[r][c] < prevHeight)):
+                return 
             visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc 
+                dfs(nr, nc, visit, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        # Running dfs on first and last row, first row -> adding to pac set, last row -> adding to atlantic set
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c]) # Adding heights that can be reached by pacific ocean
+            dfs(rows - 1, c, atl, heights[rows - 1][c]) # Adding heights that can be reached by atlantic ocean
 
-        for r in range(ROWS):
+        # Running dfs on first and last column, first column -> pacific set, last column -> atlantic set
+        for r in range(rows):
             dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
 
         res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
+        # Checking every position if it exists in both sets
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pac and (r, c) in atl: # if in both sets, that means both oceans can be reached by that position
                     res.append([r, c])
         return res
+
