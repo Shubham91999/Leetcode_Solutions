940fb2f69821481c3eff848ae721b7a68df82e0c
Shubham91999 <skulka86@asu.edu>
2025-12-31 23:04:06 -0800

    66. Plus One

--- Plus-One.py
+++ Plus-One.py
@@ -1,7 +1,26 @@
+from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = int("".join(map(str, digits)))
-        print(num+1)
-        num += 1
-        result = [int(digit) for digit in str(num)]
-        return result
+        # 1. Brute Force
+        """
+        s = ''
+        for num in digits:
+            s += str(num)
+        s = int(s)
+        s = s + 1
+        s = str(s)
+        res = []
+        for c in s:
+            res.append(int(c))
+        return res
+        """
+
+        # 2. Optimal Solution
+
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        return [1] + digits
