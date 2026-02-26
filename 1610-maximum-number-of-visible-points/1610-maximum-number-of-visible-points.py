1e339d7ff7e0866782e8feec29f00b445c49bdac
Shubham Kulkarni <156979814+Shubham91999@users.noreply.github.com>
2026-02-26 07:58:12 -0800

    Time: 315 ms (29.48%), Space: 45 MB (85.16%) - LeetHub

---
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
@@ -0,0 +1,60 @@
+class Solution:
+    def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
+        # Get angle for point from current location
+        def getAngle(p: List[int]) -> int:
+            angle = math.atan2(p[0]-location[0], p[1]-location[1]) / (2*math.pi) * 360
+
+            if angle < 0:
+                angle += 360
+
+            return angle
+
+        angles = []
+        same = 0
+
+        for p in points:
+            # point on top of location, increase same
+            if p == location:
+                same += 1
+            else:
+                angles.append(getAngle(p))
+
+        # Sorting for sliding window application
+        angles.sort()
+        # Max points that can be seen
+        ans = 0
+
+        # Queue for sliding window
+        queue = deque()
+
+        for a in angles:
+            # Added angle to sliding window queue
+            queue.append(a)
+            # Check if added angle in allowed angle frame -> a - queue[0] (starting of queue) 
+            # if this subtraction exceeds allowed angle, then we need to pop from left (starting point)
+            while a - queue[0] > angle:
+                queue.popleft()
+
+            ans = max(ans, len(queue))
+
+        for a in angles:
+            a += 360
+            queue.append(a)
+            # Check if added angle in allowed angle frame -> a - queue[0] (starting of queue) 
+            # if this subtraction exceeds allowed angle, then we need to pop from left (starting point)
+            while a - queue[0] > angle:
+                queue.popleft()
+
+            ans = max(ans, len(queue))
+
+        
+
+        return ans + same 
+
+            
+ 
+
+
+
+        
+
