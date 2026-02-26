cdf557741aebce0526a5867f7c301bedbbf99098
Shubham Kulkarni <156979814+Shubham91999@users.noreply.github.com>
2026-02-26 08:04:33 -0800

    Time: 327 ms (23.89%), Space: 46.3 MB (50.67%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
@@ -1,5 +1,7 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
+        # With double loop for angle wrap
+        """
         # Get angle for point from current location
         def getAngle(p: List[int]) -> int:
             angle = math.atan2(p[0]-location[0], p[1]-location[1]) / (2*math.pi) * 360
@@ -46,10 +48,44 @@ class Solution:
                 queue.popleft()
 
             ans = max(ans, len(queue))
+        return ans + same
+        """
 
+        # 2. Adding angles (+360) after sorting
+        def getAngle(p: List[int]) -> int:
+            angle = math.atan2(p[1]-location[1], p[0]-location[0]) / (math.pi*2) * 360 
+            if angle < 0:
+                angle += 360
+            return angle
+
+        angles = []
+        same = 0
+        # Populating angles array
+        for p in points:
+            if p == location:
+                same += 1
+            else:
+                angles.append(getAngle(p))
+
+        angles.sort()
+        angles += [a + 360 for a in angles]
+
+        # Looping through sorted angles, using sliding window to get max points
+        ans = 0
+        queue = deque()
+
+        for a in angles:
+            queue.append(a)
+
+            while a - queue[0] > angle:
+                queue.popleft()
+
+            ans = max(ans, len(queue))
+
+        return ans + same
         
 
-        return ans + same 
+
 
             
  
