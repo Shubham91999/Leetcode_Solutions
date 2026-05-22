cccc7b4b73b6e9565e1eade52096551862f6e384
Shubham Kulkarni <156979814+Shubham91999@users.noreply.github.com>
2026-05-22 07:28:18 -0700

    Time: 0 ms (100%), Space: 19.4 MB (39.04%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 1. Brute Force 
+        """
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+        """
+
+        # 2. Binary Search Approach
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            
+            # If we are in left sorted portion
+            if nums[l] <= nums[mid]:
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target <= nums[r] and target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
