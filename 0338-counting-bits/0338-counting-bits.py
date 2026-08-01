1ce4aee050425368dbea60de9595464c8cb13f40
Shubham Kulkarni <156979814+Shubham91999@users.noreply.github.com>
2026-08-01 15:51:16 -0700

    Time: 4 ms (65.35%), Space: 20.1 MB (93.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,10 +1,18 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         # Number of ones in number = 1 (from offset) + Number of ones in number (num - offset)
+        """
         dp = [0] * (n+1)
         offset = 1
         for i in range(1, n+1):
             if offset * 2 == i:
                 offset = i
             dp[i] = 1 + dp[i - offset]
+        return dp
+        """
+
+        # Solution with right shift simulating divided by 2
+        dp = [0] * (n+1)
+        for i in range(1, n+1):
+            dp[i] = dp[i >> 1] + (i & 1)
         return dp
