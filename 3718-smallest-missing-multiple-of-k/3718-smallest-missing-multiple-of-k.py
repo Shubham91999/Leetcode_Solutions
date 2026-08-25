586e3bd9498bfe4a0a5739d5d5d707ad9412eaca
Shubham Kulkarni <156979814+Shubham91999@users.noreply.github.com>
2026-08-24 23:16:47 -0700

    Time: 0 ms (100%), Space: 19.3 MB (53.35%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -1,5 +1,6 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
+        """
         nums = set(nums) # O(n)
         multiple = k
         
@@ -7,6 +8,18 @@ class Solution:
             multiple += k
             
         return multiple 
+        """
+        seen = [False] * 101
+
+        for num in nums:
+            seen[num] = True
+
+        multiple = k
+
+        while multiple <= 100 and seen[multiple]:
+            multiple += k
+
+        return multiple
 
 
         
